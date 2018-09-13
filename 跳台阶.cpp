35a38ca27e434f671eae6ec0333153d902da1d44
Qin Xuepei <qxp134@126.com>
2018-09-13 08:42:57 +0800

    7

---
+++ 跳台阶.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int jumpFloor(int number) {
+        int n = number;
+        int a[2] = {1,2};
+        if(n <= 2)return a[n-1];
+        int ans;
+        for(int i = 3;i <= n;i++){
+            ans = a[0] + a[1];
+            a[0] = a[1];
+            a[1] = ans;
+        }
+        return ans;
+    }
+};
