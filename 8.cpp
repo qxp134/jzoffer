9582ad5c77269a8642e2b87bcdb4666aed914461
Qin Xuepei <qxp134@126.com>
2020-06-15 22:53:02 +0800

    modify

--- 8.cpp
+++ 8.cpp
@@ -1,42 +1,30 @@
 // https://leetcode-cn.com/problems/string-to-integer-atoi/submissions/
 #include <iostream>
 #include <string>
-using namespace std;
 class Solution {
  public:
   int myAtoi(string str) {  //第一遍失败了，最后结果可能long long都放不下
-    int positive_num = 0;
+    bool positive_flag = true;
     long long res = 0;
-    auto i = str.begin();
-    for (; i != str.end(); i++)  //开头空白去掉
-    {
-      if (*i == ' ') {
-        continue;
-      } else {
-        if (*i == '+') {
-          positive_num = 1;
-          i++;
-        } else if (*i == '-') {
-          positive_num = 2;
-          i++;
-        } else if (*i < '0' || *i > '9')
-          return 0;
+    auto iter = str.begin();
+    for (; iter != str.end() && *iter == ' '; ++iter)  //开头空白去掉
+    	;
+    if (iter == str.end()) return 0;
+    if (*iter == '+') ++iter;
+    else if (*iter == '-') {
+      positive_flag = false;
+      ++iter;
+    }
+    string res_str(iter, str.end());
+    for (iter = res_str.begin(); iter != res_str.end(); ++iter) {
+      if (*iter < '0' || *iter > '9') {
         break;
       }
-    }
-    if (i == str.end()) {
-      return 0;
-    }
-    string res_str(i, str.end());
-    for (i = res_str.begin(); i != res_str.end(); i++) {
-      if (*i < '0' || *i > '9') {
-        break;
-      }
-      res = res * 10 + (*i - '0');
+      res = res * 10 + (*iter - '0');
       if (res > INT_MAX)
         break;
     }
-    if (positive_num == 2) {
+    if (!positive_flag) {
       res = -res;
       if (res < INT_MIN) {
         return INT_MIN;
