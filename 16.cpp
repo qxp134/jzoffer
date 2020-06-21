8ab4573d3f82ac98864ab43791d11c8c271c9acc
Qin Xuepei <qxp134@126.com>
2020-06-21 22:44:11 +0800

    fix16

--- 16.cpp
+++ 16.cpp
@@ -1,21 +1,20 @@
 // https://leetcode-cn.com/problems/3sum-closest/
-#include <limits.h>
-
 #include <algorithm>
 #include <iostream>
 #include <vector>
 
 class Solution {
  public:
-  int threeSumClosest(std::vector<int>& nums, int target) {
-    sort(nums.begin(), nums.end());
+  int threeSumClosest(const std::vector<int>& nums, int target) {
+    std::sort(nums.begin(), nums.end());
     int last_num = 999;
 
     int len = nums.size();
-    int ans = nums[0] + nums[len - 1] + nums[len - 2];
-    int min_diff = abs(target - ans);
+    int best_ans = nums[0] + nums[len - 1] + nums[len - 2];
+    int min_diff = std::abs(target - best_ans);
     for (int i = 0; i < len; ++i) {
       if (nums[i] == last_num) {
+        last_num = nums[i];
         continue;
       }
       int j = i + 1;
@@ -31,11 +30,11 @@ class Solution {
           return target;
         }
         if (abs(diff) < min_diff) {
-          ans = target + diff;
+          best_ans = target + diff;
           min_diff = abs(diff);  // frist commit I forget abs
         }
       }
     }
-    return ans;
+    return best_ans;
   }
 };
--- 17.cpp
+++ 17.cpp
@@ -2,49 +2,41 @@
 #include <iostream>
 #include <string>
 #include <vector>
-std::vector<std::string> res;
-std::vector<std::string> recursion(std::string digits) {
-  if (digits.empty()) return res;
-  std::string::iterator it = digits.begin();
-  int count = 3;
-  if (*it != '1') {
-    if (*it == '7' || *it == '9') {  // forget num '7'
-      count = 4;
-    }
-    std::vector<std::string> temp_arr[4];
-    char start_char;
-    if (*it < '8') {
-      start_char = (*it - '2') * 3 + 'a';
-    } else {
-      start_char = (*it - '8') * 3 + 't';
-    }
 
-    for (int i = 0; i < count; ++i) {
-      temp_arr[i] = res;
-      if (temp_arr[i].empty()) {
-        std::string temp_str(1, start_char);
-        temp_arr[i].push_back(temp_str);
-      } else {
-        for (auto iter = temp_arr[i].begin(); iter != temp_arr[i].end();
-             ++iter) {
-          iter->push_back(start_char);
-        }
-      }
-      start_char += 1;
-    }
-    res.clear();
-    for (int i = 0; i < count; ++i) {
-      res.insert(res.end(), temp_arr[i].begin(), temp_arr[i].end());
-    }
-  }
-  digits.erase(it);
-  return recursion(digits);
-}
 class Solution {
  public:
   std::vector<std::string> letterCombinations(std::string digits) {
-    std::vector<std::string> ans = recursion(digits);
-    res.clear();
-    return ans;
+    return recursion(digits);
   }
+  std::vector<std::string> recursion(std::string digits) {
+    if (digits.empty())
+      return letter_combinations;
+    std::string::iterator it = digits.begin();
+    if (number_to_chars[*it - '0'] != "") {
+      char start_char = number_to_chars[*it - '0'][0];
+      int count = number_to_chars[*it - '0'].size();
+      std::vector<std::string> temp_arr[4];
+      for (int i = 0; i < count; ++i) {
+        temp_arr[i] = letter_combinations;
+        if (temp_arr[i].empty()) {
+          std::string temp_str(1, start_char);
+          temp_arr[i].push_back(temp_str);
+        } else {
+          for (std::string& str : temp_arr[i]) str.push_back(start_char);
+        }
+        start_char += 1;
+      }
+      letter_combinations.clear();
+      for (int i = 0; i < count; ++i) {
+        letter_combinations.insert(letter_combinations.end(), temp_arr[i].begin(), temp_arr[i].end());
+      }
+    }
+    digits.erase(it);
+    return recursion(digits);
+  }
+
+ private:
+  std::vector<std::string> letter_combinations;
+  std::string number_to_chars[10] = {"",    "",    "abc",  "def", "ghi",
+                                     "jkl", "mno", "pqrs", "tuv", "wxyz"};
 };
